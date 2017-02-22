8b16ea0e93581410d2f953e2d5c30d0d9b4b8a1b
weibo <bo.wei@healthbok.com>
2017-02-22 17:58:15 +0800

    first commit

---
+++ app/src/main/java/com/example/component/demo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.component.demo;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+
+import com.example.component.demo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.recycler.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
